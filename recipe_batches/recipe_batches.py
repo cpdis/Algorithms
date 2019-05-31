9299bb4c9ec3fdffecd92358bbf1ab5ff7d67c0c
Colin Dismuke <colin.dismuke@gmail.com>
2019-05-30 20:32:29 -0500

    Added recipe_batches solution

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,19 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # this uses a couple of built-in Python functions to reduce the amount of code needed
+    # items() gets the keys and values for the recipe dictionary and get() returns the
+    # value for the specified key if it's in the dictionary (0 otherwise) and divides it
+    # by the amount required by the recipe
+    return min(int(ingredients.get(ingredient, 0) / amount) for ingredient, amount in recipe.items())
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
