6934e1760907c4d21074de05499d1d74d363f06d
Colin Dismuke <colin.dismuke@gmail.com>
2019-05-30 20:21:10 -0500

    Add naive stock_prices solution

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,28 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    max_profit = 0
+
+    # this loops through the array of stock prices subtracting the initial price (price)
+    # from those that come after (price_2)
+    for index, price in enumerate(prices):
+        for price_2 in prices[index+1:]:
+            profit = price_2 - price
+
+            if max_profit == 0 or profit > max_profit:
+                max_profit = profit
+    return max_profit
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
